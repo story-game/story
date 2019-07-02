08a61cf895679724a7179460e8582e69e130ea35
Nícolas 'nohaleh' Deçordi <eowfenth.services@gmail.com>
2019-07-02 20:17:03 -0300

    feat: add initial movement to character

---
+++ KinematicBody2D.gd
@@ -0,0 +1,27 @@
+extends KinematicBody2D
+
+const UP: Vector2 = Vector2(0, -1)
+const GRAVITY = 20
+const SPEED = 200
+const JUMP_HEIGHT = -500
+var motion: Vector2 = Vector2()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	motion.y += GRAVITY
+
+	if (Input.is_action_pressed("ui_right")):
+		motion.x = SPEED
+	elif (Input.is_action_pressed("ui_left")):
+		motion.x = -SPEED
+	elif (Input.is_action_just_pressed("ui_down")):
+		motion.y = 2 * SPEED
+	else:
+		motion.x = 0
+
+	if (is_on_floor()):
+		if (Input.is_action_just_pressed("ui_up")):
+			motion.y = JUMP_HEIGHT
+			
+	motion = move_and_slide(motion, UP)
+	pass
